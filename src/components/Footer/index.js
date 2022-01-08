08ace2bc3aa6366b4533262b733d8a24fbdd81c7
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-07 21:10:37 -0500

    basic homepage with links to sub pages in place

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Footer() {
+
+  return (
+    <footer className="flex-row px-1">
+      <h3>
+        Made with ❤!
+      </h3>
+
+    </footer>
+  )
+}
+
+export default Footer;
