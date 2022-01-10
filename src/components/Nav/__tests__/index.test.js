6cb7d6d090b75af7ce428fcd2380dfc294e73091
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-09 19:28:38 -0500

    replace app.js

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, cleanup} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from "..";
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+  it('renders', () => {
+    render(<Nav/>)
+  });
+});
+
+it('matches snapshot', () => {
+  const { asFragment } = render(<Nav/>);
+  expect(asFragment()).toMatchSnapshot();
+});
