63313faf731028d1b48e9049de816f466a2d0692
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-15 21:56:46 -0500

    trying to resolve react problems

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+
+
+const About = () => {
+  return 
+  <section>
+    <h1>Hello</h1>
+  </section>
+
+}
+
+export default About;
