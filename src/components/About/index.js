4816473f0513b9ff8f7b21cb7cd28fc1804eef96
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-15 23:13:56 -0500

    basic about me page set up. needs content, fill in once all pages work

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { capitalizeFirstLetter } from '../../utils/helpers';
+import coverImage from "../../assets/head-shot/018.jpg"
 
 
 
 const About = () => {
   return (
-  <section>
+  <section className="row justify-content-start">
+    <div className="col-6">
     <h1>Hello</h1>
+    <img src={coverImage} className="my-2" style={{ width: "75%" }} alt="Kendall Felder" />
+    </div>
+    <div className="col-6">
+      kdfjlajdfklajldkfjlajdklfjaldjflajdfljaldjflajdfljaldfjl
+    </div>
   </section>
   );
 
