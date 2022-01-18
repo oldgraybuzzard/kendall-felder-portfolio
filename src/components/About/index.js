532a3f846d06f283dc0b69edfeba601622215504
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-18 08:01:20 -0500

    add photo to about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { capitalizeFirstLetter } from '../../utils/helpers';
+import headShot from '../../assets/head-shot/018.jpg';
 
 
 
@@ -15,13 +16,16 @@ const About = () => {
   return (
     <div className="masthead text-center text-white d-flex">
             <div className="container my-auto">
-                <div className="row">
-                    <div className="col-lg-10 mx-auto">
-                        <h1>{capitalizeFirstLetter(currentPage.name)}</h1>
-                        <hr />
-                        <p className="text-faded mb-5">I am an experienced front end developer with detailed full stack knowledge. I recently earned a Certificate in Full Stack Web Development from the University of Central Florida, where I developed skills in MERN Stack and SQL Server.</p>
-                       
-                        <p className="text-faded mb-5">My strong organizational skills and excellent problem-solving abilities allow me to effectively collaborate on cross-functional teams that include designers, software developers, scrum masters, and project managers. My advanced education, professional background in technology, drive for success, and strong attention to detail are great assets that I own.</p>
+                <div className="col">
+                    <img src={headShot} style={{ width: "15%", borderRadius: '100px' }} alt="kendall felder"></img>
+                    <div className="row">
+                        <div className="col-lg-10 mx-auto">
+                            <h1>{capitalizeFirstLetter(currentPage.name)}</h1>
+                            <hr />
+                            <p className="text-faded mb-5">I am an experienced front end developer with detailed full stack knowledge. I recently earned a Certificate in Full Stack Web Development from the University of Central Florida, where I developed skills in MERN Stack and SQL Server.</p>
+                        
+                            <p className="text-faded mb-5">My strong organizational skills and excellent problem-solving abilities allow me to effectively collaborate on cross-functional teams that include designers, software developers, scrum masters, and project managers. My advanced education, professional background in technology, drive for success, and strong attention to detail are great assets that I own.</p>
+                        </div>
                     </div>
                 </div>
             </div>
